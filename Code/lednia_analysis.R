2ab123eede768518e673b8a44c725ba9a4feeb7c
mgree013 <mgree013@ucr.edu>
2021-09-30 10:59:23 -0600

    update and load

---
+++ Code/lednia_analysis.R
@@ -0,0 +1,126 @@
+#Project: Lednia Review Paper Analysis
+#Title:Stoneflies in the genus Lednia (Plecoptera: Nemouridae): sentinels of climate change impacts on mountain stream biodiversity
+#journal: Biodiversity and Conservation
+#Date: Sept 30,2021
+
+######################################################
+library(tidyverse)
+library(ggplot2)
+library(cowplot)
+######################################################
+
+data<-read.csv("Data/Lednia.analysis.csv")
+######################################################
+
+#Figure 4:
+elev.sp<-data%>%
+  ggplot(aes(x=Species,y=Elev.m, fill=Species))+
+  geom_boxplot()+
+  ylab("Elevation (m)")+ theme_bw()+
+  theme(panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank(),
+        panel.border = element_rect(colour = "black"))+
+  theme(legend.position = "none")
+
+dist.sp<-data%>%
+  filter(D.From.Source.m< 5000)%>%
+  ggplot(aes(x=Species,y=D.From.Source.m, fill=Species))+
+  geom_boxplot()+
+  ylab("Distance from Source (m)")+ theme_bw()+
+  theme(panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank(),
+        panel.border = element_rect(colour = "black"),
+        legend.box.background = element_rect(colour = "black", linetype='solid'))+
+  theme(legend.position = c(.6,.8))
+
+dist.genus<-data%>%
+  ggplot(aes(x=Genus,y=D.From.Source.m, fill=Genus))+
+  geom_boxplot()+
+  ylab("Distance from Source (m)")
+
+elev.genus<-data%>%
+  ggplot(aes(x=Genus,y=Elev.m, fill=Genus))+
+  geom_boxplot()+
+  ylab("Elevation (m)")+ theme_bw()+
+  theme(panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank(),
+        panel.border = element_rect(colour = "black"))+
+  theme(legend.position = "none")
+
+
+plot_grid(elev.sp,dist.sp)
+
+elev.anova<-aov(Elev.m~Species, data=data)
+summary(elev.anova)
+TukeyHSD(elev.anova)
+
+sd(data$D.From.Source.m)
+stderr(data$D.From.Source.m)
+dist.anova<-aov(D.From.Source.m~Species, data=data)
+summary(dist.anova)
+TukeyHSD(dist.anova)
+
+##########################################################################################
+#Figure 3
+time.pops<-data%>%
+  filter(Species !="Lednia tumana")%>%
+  group_by(Year)%>%
+  add_column(Pops=1)%>%
+  transmute(number.pops=sum(Pops))
+  
+time.pops%>%
+  ggplot(aes(x=Year)) + 
+  geom_bar()
+
+time.pops%>%
+  ggplot(aes(x=Year,y=number.pops)) + 
+  geom_point()+
+  ylab("Elevation (m)")+ theme_bw()+
+  theme(panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank(),
+        panel.border = element_rect(colour = "black"))+
+  theme(legend.position = "none")
+  
+  ########################################################################################################################  
+#Population discovery
+  
+#by species
+new_data<-read.csv("Data/lednia_new_data.csv")
+
+new_dataz<-new_data%>%
+  add_column(number=1)%>%
+  group_by(Species)%>%
+  mutate(total.number.pops=sum(number))%>%
+  ungroup()%>%
+  group_by(Species, Year,total.number.pops)%>%
+  summarise(number.pops=sum(number))%>%
+  ungroup()%>%
+  drop_na()
+
+new_dataz%>%
+  ggplot(aes(x=Year, y=number.pops, colour=Species))+
+  geom_point()+
+  geom_line()
+
+#write.csv(new_dataz, "new_dataz.csv")  
+newer_dataz<-read.csv("Data/new_dataz.csv")
+
+newer_dataz%>%
+  ggplot(aes(x=Year, y=new.pops, colour=Species))+
+  geom_point()+
+  geom_line()+
+  ylab("Cumulative Number of Popualtions Discovered")+
+  theme_bw()+
+  theme(panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank(),
+        panel.border = element_rect(colour = "black"))
+
+#all lednia
+
+new_datazz<-new_data%>%
+  add_column(number=1)%>%
+  summarise(number.pops=sum(number))%>%
+  ungroup()%>%
+  drop_na()
+
+write.csv(new_datazz, "new_datazx.csv") 
